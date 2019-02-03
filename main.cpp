0d3e0c7b8ae437f8bc8f1d10b71dd64edaea1613
karanchahal <karanbleep@gmail.com>
2019-02-03 07:32:29 +0530

    Basic backprop working

--- main.cpp
+++ main.cpp
@@ -2,67 +2,29 @@
 
 using namespace std; 
 
-// graph has nodes and egdes
-// our edges will be tensors
-// our nodes will be operations
-
-class Operation {
-
-};
-
-// class MultiplyOperation {
-
-// };
-
-// class AddOperation {
-
-// };
-
-class Tensor {
-
-};
-
-// after handling basic numbers
-// we come to matrices. 
-// I can also you how to do really fast matrix multiplcations using CUDA , put it on the GPU
-
-// train a network hopefully. Maybe MNIST
-
-// build python warppaers around out C++ api
+class FloatTensor;
 
 class MultiplyOperation {
 
 
     public:
-        FloatTensor t1, t2; // generally an operation had two operands
+        FloatTensor *t1, *t2; // generally an operation had two operands
     
-    void backward(Gradient grad) {
-        // switcher,
-        t1.backward(grad*t2.val);
-        t1.backward(grad*t1.val);
+    MultiplyOperation(FloatTensor *t1, FloatTensor *t2) {
+        this->t1 = t1;
+        this->t2 = t2;
     }
+    void backward(float grad);
 
-    void compute() {
-        return new Floattensor(t1.val*t2.val, backwardOperation=this)
-    }
-}
+    FloatTensor compute();
+};
 
-
-class AddOperation {
-
-    public:
-        FloatTensor t1, t2; // generally an operation had two operands
-    void backward(Gradient grad) {
-        // distributor
-        t1.backward(grad);
-        t1.backward(grad);
-    }
-}
 
 class FloatTensor {
     public:
     float val; // value of tensor 
     float grad; // value of grad
+    MultiplyOperation *frontOp =NULL, *backOp =NULL;
 
     FloatTensor() {
         // default
@@ -72,31 +34,49 @@ class FloatTensor {
         this->val = val;
     }
 
- 
-
-    void backward(Gradient grad) {
-        this->grad = grad;
-        // this->val = this->val -learning_rate*grad (gradient decent)
-
-        if(this->backwardOperation != NULL)
-            this->backwardOperation.backward(grad); // this would be None so stop here
+    FloatTensor(float val, MultiplyOperation* op) {
+        this->val = val;
+        this->backOp = op;
     }
 
-    FloatTensor* operator * (FloatTensor* two) { 
-        this->forwardOperation = MultiplyOperation(this, two)
+    void backward(float grad) {
+        this->grad = grad;
+        if(this->backOp != NULL) {
+            this->backOp->backward(grad);
+        }
+    }
 
-        return this->forwardOperation.compute();
+
+    FloatTensor operator * (FloatTensor* two) { 
+        this->frontOp = new MultiplyOperation(this, two);
+        return this->frontOp->compute();
     }
 };
+
+
+void MultiplyOperation::backward(float grad) {
+    // cout<<this->t2->val<<endl;
+    // Swithing case: weherre gradients are multiplied with the opposite tensor
+    this->t1->backward(grad*this->t2->val);
+    this->t2->backward(grad*this->t1->val);
+}
+
+FloatTensor MultiplyOperation::compute() {
+    return FloatTensor(this->t1->val*this->t2->val, this);
+}
 
 
 // vector Tensor 
 
 int main() {
-    // and autodiff library
-    FloatTensor* one = new FloatTensor(2);
-    FloatTensor* two = new FloatTensor(4);
 
-    FloatTensor* three = one*two;
+    FloatTensor one(2); // heap declaration
+    FloatTensor two(4);
+    
+    FloatTensor three = one*(&two);
     three.backward(1);
+
+    cout<<three.grad<<endl; // 1
+    cout<<one.grad<<endl; // 1*4 = 4
+    cout<<two.grad<<endl; // 1*2 = 2
 }
