71633e72dac9dfa77e63168ec117807dedfa1193
karanchahal <karanbleep@gmail.com>
2019-04-12 13:50:20 +0530

    Loaded mnist

--- main.cpp
+++ main.cpp
@@ -1,28 +1,38 @@
 #include "buildTensorflow.h"
-
+#include "mnist/include/mnist/mnist_reader.hpp"
 // Example of training a network on the buildTensorflow framework.
 int main() {
-    
-    // Load Dataset
-    Celsius2Fahrenheit<float,float> dataset;
-    dataset.create(5);
+
+    // Load MNIST Dataset
+    auto dataset = mnist::read_dataset<std::vector, std::vector, float, uint8_t>();
+    auto train_images = dataset.training_images;
+    auto train_labels = dataset.training_labels;
+    auto test_images = dataset.test_images;
+    auto test_labels = dataset.test_labels;
+
 
     // Create Model
-    Dense<float> fc1(1,1,NO_ACTIVATION);
+    Dense<float> fc1(784,100);
+    Dense<float> fc2(100,20);
+    Dense<float> fc3(20,1, NO_ACTIVATION);
 
     // Initialise Optimiser
-    SGD<float> sgd(0.01);
+    SGD<float> sgd(0.0001);
     
     // Train
-    cout<<"Training started"<<endl;
-    for(int j = 0;j<2000;j++) {
-        for(auto i: dataset.data) {
+
+    float loss_till_now = 0;
+    for(int j = 0;j<1;j++) {
+        int ld = 0;
+        for(auto i: train_images) {
             // Get data
-            auto inp = new Tensor<float>({i.first}, {1,1});
-            auto tar = new Tensor<float>({i.second}, {1,1});
+            auto inp = new Tensor<float>({i}, {1,784});
+            auto tar = new Tensor<float>({(float)train_labels[ld]}, {1,1});
 
             // Forward Prop
-            auto out = fc1.forward(inp);
+            auto temp = fc1.forward(inp);
+            auto temp2 = fc2.forward(temp);
+            auto out = fc3.forward(temp2);
 
             // Get Loss
             auto l = new Tensor<float>({-1}, {1,1});
@@ -35,20 +45,31 @@ int main() {
 
             // Perform Gradient Descent
             sgd.minimise(finalLoss);
+            loss_till_now += finalLoss->val.val[0];
+
+            if(ld%2000 == 0) {
+                cout<<loss_till_now/ld<<endl;
+            }
+
+            ld++;
         
         }
     }
 
-    cout<<"Training completed"<<endl;
-
     // Inference
-    float cel = 4;
-    auto test = new Tensor<float>({cel}, {1,1});
-    auto out1 = fc1.forward(test);
+    auto testVal = test_images[0];
+    auto test = new Tensor<float>({testVal}, {1,784});
+    auto temp = fc1.forward(test);
+    auto temp2 = fc2.forward(temp);
+    auto ans = fc3.forward(temp2);
 
-    cout<<"The conversion of "<<cel<<" degrees celcius to fahrenheit is "<<out1->val<<endl; // For 4 Celcius: it's ~39.2
+    cout<<ans->val<<endl;
+    cout<<(float)test_labels[0]<<endl;
 
-    // Clean up
-    delete out1;
+    // ASSERT_TRUE(testUtils::isMatrixEqual(ans->val,res));
+
+    // // Clean up
+    // delete ans;
+    
 }
 
