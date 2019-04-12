20c48fcbcacbed1805f095a4dd485cab67a0802d
karanchahal <karanbleep@gmail.com>
2019-04-12 14:29:42 +0530

    Embed gpu code in matrix

--- buildTensorflowGpu.h
+++ buildTensorflowGpu.h
@@ -1,9 +1,9 @@
 // Check whether GPU is accessible or not
 bool gpu = true;
 
-#include "gpu/defn.h" // Includes GPU Kernel Code Defination for Forward pass
+// #include "gpu/defn.h" // Includes GPU Kernel Code Defination for Forward pass
 #include "types/tensor.h"
-#include "gpu/impl.h" // Includes GPU Kernel Code Implementation
+// #include "gpu/impl.h" // Includes GPU Kernel Code Implementation
 #include "overloads/tensor.h"
 #include "operations/operations_Impl.h"
 #include "layers/dense.h"
